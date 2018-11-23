2508e04cd502a9fd96e6351eb5628c95ee5f885d
zhang <1820128171@qq.com>
2018-11-23 17:22:41 +0800

    "新增了一个controller"

---
+++ src/com/tecode/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.tecode.controller;
+
+public class UserController {
+    public static void main(String[] args) {
+        UserController us=new UserController();
+        us.add();
+    }
+    public void add(){
+        System.out.println("我又进来了");
+    }
+}
